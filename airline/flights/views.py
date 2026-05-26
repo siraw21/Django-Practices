a59a044a6727ae7418866660774f6e5272896950
Sirawdink Abayneh <sirawdinkabayneh77@gmail.com>
2026-05-26 18:04:10 +0300

    createDjango Adimnistration

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Flight
 
 
@@ -8,4 +7,10 @@ from .models import Flight
 def index(request):
   return render(request, "flights/index.html", {
     "flights": Flight.objects.all()
+  })
+
+def flight(request, flight_id):
+  flight = Flight.objects.get(pk=flight_id)
+  return render(request, "flights/flight.html", {
+    "flight": flight
   })
