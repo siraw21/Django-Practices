6e3c33197771d231cdb408e45c981521a1978922
Sirawdink Abayneh <sirawdinkabayneh77@gmail.com>
2026-05-26 17:38:21 +0300

    add flights app

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight
+
+
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Welcome To Sims")
+  return render(request, "flights/index.html", {
+    "flights": Flight.objects.all()
+  })
